12f095301253d6693b8e557f86417326f1be94db
rcVIKASH <92142730+rcVIKASH@users.noreply.github.com>
2025-04-26 19:04:57 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors());  //{ origin: process.env.CORS_ORIGIN, credentials: true }
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true })); 
 app.use(express.json({ limit: "64kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public")); //v1
