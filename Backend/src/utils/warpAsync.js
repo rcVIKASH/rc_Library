c90d8fff1b8a3a21f547cab656e76ae278ffc161
rcVIKASH <92142730+rcVIKASH@users.noreply.github.com>
2025-04-26 17:26:25 +0530

    Delete Backend directory

--- Backend/src/utils/warpAsync.js
+++
@@ -1,9 +0,0 @@
-// utils/wrapAsync.js
-
-const wrapAsync = (fn) => {
-  return function (req, res, next) {
-    fn(req, res, next).catch(next); // pass errors to Express error handler
-  };
-};
-
-export { wrapAsync };
